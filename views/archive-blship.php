f42d4edcfbec145175e0a1c8fcdfff79a6f72758
Ben Lobaugh <ben@lobaugh.net>
2012-09-04 16:05:46 -0700

    Updated templates

--- views/archive-blship.php
+++ views/archive-blship.php
@@ -1,5 +1,17 @@
 <?php get_header(); ?>
 
-archive-blship
+<?php while ( have_posts() ) : the_post(); ?>
+<article <?php post_class(); ?>>
+<h2><b><a href="<?php the_permalink(); ?>">  <?php the_title(); ?> </a></b></h2>
+<?php 
+
+    $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail'); 
+    echo '<img src="' . $large_image_url[0] . '" />';
+    
+?>
+<?php the_excerpt(); ?>
+
+</article>
+<?php endwhile; // end of the loop. ?>
 
 <?php get_footer(); ?>
